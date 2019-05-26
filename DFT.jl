1ae765b1e3587bd12f0c58a157a175292f25580b
kuboshiba <r115313@g.kobe-kosen.ac.jp>
2019-05-26 21:13:55 +0900

    離散フーリエ変換

--- DFT.jl
+++ DFT.jl
@@ -1,13 +1,12 @@
-signal = [1, -1, 5, -3, 1, -3, 0, 0]
+signal = [1, -1, 5, -3, 1, -3, 0, 0] #入力信号
 
 N = length(signal)
 t = 2pi / N
 
-Re, Im = [], []
+Re, Im = Float64[], Float64[]
 
-# DFT
 for i in 1:N
-    re, im = 0, 0
+    re, im = 0.0, 0.0
     for j in 1:N
         re += signal[j] * cos(t*(i-1)*(j-1))
         im += -signal[j] * sin(t*(i-1)*(j-1))
